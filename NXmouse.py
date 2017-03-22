041d9b66151484df586857eef815b5f818493a20
ooblog <ooblog@gmail.com>
2017-03-22 22:28:05 +0900

    「xdotool mousemove_relative --polar」を複数指定できるように「NXmouse.tsv」で「NXkbd」ページを独立。HJKL移動をかな表現使わず実装するために「LTsv/LTsv_kbd.tsv」の特殊キーを小文字に揃える。

--- NXmouse.py
+++ NXmouse.py
@@ -13,47 +13,56 @@ from LTsv_file   import *
 from LTsv_kbd    import *
 from LTsv_gui    import *
 
-NXmouse_ltsvname,NXmouse_ltsvtext,NXmouse_config="NXmouse.tsv","",""
-NXmouse_windowW,NXmouse_windowH=320,120
+NXmouse_ltsvname,NXmouse_ltsvtext,NXmouse_config,NXkbd_config="NXmouse.tsv","","",""
+NXmouse_windowW,NXmouse_windowH=320,240
 NXmouse_notifyname="NXmouse.png"
 NXmouse_wait=1000
 NXmouse_KBD=0
-NXmouse_kbdLCR=["NFER","None","XFER"]
-NXmouse_nameLCR=["kbdL","kbdC","kbdR"]
-NXmouse_downLCR=["xdotool mousedown 1","xdotool mousedown 2","xdotool mousedown 3"]
-NXmouse_upLCR=["xdotool mouseup 1","xdotool mouseup 2","xdotool mouseup 3"]
-NXmouse_keepLCRbf=list(tuple(NXmouse_downLCR[:]))
-NXmouse_keepLCRaf=list(tuple(NXmouse_downLCR[:]))
-NXmouse_iconnameBF,NXmouse_iconnameAF="",""
+NXmouse_NXkbd={}
+NXmouse_mouseLCRcount=3
+NXmouse_keepLCRaf=[False for mousecount in range(NXmouse_mouseLCRcount+1)]
+NXmouse_keepLCRbf=[False for mousecount in range(NXmouse_mouseLCRcount+1)]
 
+NXmouse_getkbdnamesBF,NXmouse_getkbdnamesAF="",""
 def NXmousenotify_timeK(callback_void=None,callback_ptr=None):
-    global NXmouse_iconnameBF,NXmouse_iconnameAF
-    LTsv_setkbddata(20,10); glyphtype_getkbdnames=LTsv_getkbdnames()
-    NXmouse_iconnameAF=glyphtype_getkbdnames
-    if NXmouse_KBD != 0 and NXmouse_iconnameBF != NXmouse_iconnameAF:
-        for kbdcount,kbdLCR in enumerate(NXmouse_kbdLCR):
-            if kbdLCR in NXmouse_iconnameAF:
-                NXmouse_keepLCRaf[kbdcount]=NXmouse_downLCR[kbdcount]
+    global NXmouse_getkbdnamesBF,NXmouse_getkbdnamesAF
+    global NXmouse_keepLCRbf,NXmouse_keepLCRaf
+    LTsv_setkbddata(20,10); NXmouse_getkbdnamesAF=LTsv_getkbdnames()
+    NXmouse_kbdbuf=""
+    if NXmouse_KBD != 0 and NXmouse_getkbdnamesBF != NXmouse_getkbdnamesAF:
+        for NXcmd,NXkbd in NXmouse_NXkbd.items():
+            for kbd in NXkbd:
+                if not kbd in NXmouse_getkbdnamesAF: break;
             else:
-                NXmouse_keepLCRaf[kbdcount]=NXmouse_upLCR[kbdcount]
-            if NXmouse_keepLCRbf[kbdcount] != NXmouse_keepLCRaf[kbdcount]:
-                LTsv_subprocess(NXmouse_keepLCRaf[kbdcount])
-                NXmouse_keepLCRbf[kbdcount] = NXmouse_keepLCRaf[kbdcount]
-        LTsv_widget_settext(NXmouse_window,widget_t="NXmouse:"+NXmouse_iconnameAF)
-        NXmouse_iconnameBF=NXmouse_iconnameAF
+                NXcmds=NXcmd.split(' ')
+                if NXcmds[0] == "updown":
+                    NXmouse_keepLCRaf[int(NXcmds[1])]=True
+                elif NXcmds[0] == "polar":
+                    LTsv_subprocess("xdotool mousemove_relative --polar {0} {1}".format(NXcmds[1],NXcmds[2]))
+    for mousecount in range(1,NXmouse_mouseLCRcount+1):
+        if NXmouse_keepLCRbf[mousecount] != NXmouse_keepLCRaf[mousecount]:
+            NXmouse_keepLCRbf[mousecount]=NXmouse_keepLCRaf[mousecount]
+            if NXmouse_keepLCRaf[mousecount]:
+                 LTsv_subprocess("xdotool mousedown {0}".format(mousecount))
+            else:
+                 LTsv_subprocess("xdotool mouseup {0}".format(mousecount))
+        NXmouse_keepLCRaf[mousecount]=False
+    LTsv_widget_settext(NXmouse_window,widget_t="NXmouse:"+NXmouse_kbdbuf)
     LTsv_window_after(NXmouse_window,event_b=NXmousenotify_timeK,event_i="NXmousenotify_timeK",event_w=NXmouse_wait)
 
 def NXmouse_configload():
     global NXmouse_ltsvname,NXmouse_ltsvtext,NXmouse_config
     global NXmouse_notifyname,NXmouse_wait,NXmouse_KBD
-    global NXmouse_kbdLCR
+    global NXkbd_config,NXmouse_NXkbd
     NXmouse_ltsvtext=LTsv_loadfile(NXmouse_ltsvname)
     NXmouse_config=LTsv_getpage(NXmouse_ltsvtext,"NXmouse")
+    NXmouse_wait=min(max(LTsv_intstr0x(LTsv_readlinerest(NXmouse_config,"wait",str(NXmouse_wait))),10),1000)
     NXmouse_notifyname=LTsv_readlinerest(NXmouse_config,"notify",NXmouse_notifyname)
-    NXmouse_wait=min(max(LTsv_intstr0x(LTsv_readlinerest(NXmouse_config,"wait",str(NXmouse_wait))),20),1000)
     NXmouse_KBD=min(max(LTsv_intstr0x(LTsv_readlinerest(NXmouse_config,"KBD",str(NXmouse_KBD))),0),1)
-    for namecount,nameLCR in enumerate(NXmouse_nameLCR):
-        NXmouse_kbdLCR[namecount]=LTsv_readlinerest(NXmouse_config,nameLCR,NXmouse_kbdLCR[namecount])
+    NXkbd_config=LTsv_getpage(NXmouse_ltsvtext,"NXkbd")
+    for NXkbd_configline in NXkbd_config.split('\n'):
+        if len(NXkbd_configline) == 0 or not '\t' in NXkbd_configline: continue;
+        NXmouse_NXkbd[NXkbd_configline.split('\t')[0]]=NXkbd_configline.split('\t')[1:]
 
 def NXmouse_menu():
     yield ("「NXmouse」の終了",NXmouse_exit_cbk)
@@ -64,12 +73,12 @@ def NXmouse_menu():
 
 def NXmouse_view(KBD=None):
     global NXmouse_KBD
+    global NXmouse_keepLCRbf,NXmouse_keepLCRaf
     if KBD != None:
         NXmouse_KBD=min(max(KBD,0),1)
-        for kbdcount,kbdLCR in enumerate(NXmouse_kbdLCR):
-            NXmouse_keepLCRaf[kbdcount]=NXmouse_upLCR[kbdcount]
-            LTsv_subprocess(NXmouse_keepLCRaf[kbdcount])
-            NXmouse_keepLCRbf[kbdcount] = NXmouse_keepLCRaf[kbdcount]
+        for mousecount in range(NXmouse_mouseLCRcount):
+            NXmouse_keepLCRbf[mousecount],NXmouse_keepLCRaf[mousecount]=False,False
+            LTsv_subprocess("xdotool mouseup {0}".format(mousecount+1))
     LTsv_widget_seturi(NXmouse_notifyicon,widget_u="{0}[{1}]".format(NXmouse_notifyname,NXmouse_KBD))
     LTsv_widget_settext(NXmouse_notifyicon,widget_t="NXmouse")
 
@@ -100,6 +109,7 @@ if len(LTsv_GUI) > 0:
         NXmouse_iconnameAF="NXmouse"
         NXmouse_window=LTsv_window_new(event_b=LTsv_hideondelete,widget_t=NXmouse_iconnameAF,widget_w=NXmouse_windowW,widget_h=NXmouse_windowH)
         NXmouse_configload()
+        LTsv_label_new(NXmouse_window,widget_t=NXkbd_config,widget_x=0,widget_y=0,widget_w=NXmouse_windowW,widget_h=NXmouse_windowH)
         LTsv_draw_picture_load(NXmouse_notifyname); LTsv_draw_picture_celldiv(NXmouse_notifyname,2,1)
         NXmouse_notifyicon=LTsv_notifyicon_new(NXmouse_window,widget_t=NXmouse_iconnameAF,widget_u="{0}[{1}]".format(NXmouse_notifyname,NXmouse_KBD),menu_b=NXmouse_menu(),menu_c=NXmouse_switch_cbk)
 #        LTsv_widget_showhide(NXmouse_window,True)
